b5535f1d5822960cddaef2a924a3375abe45957e
TaitA2 <adam.tait617@gmail.com>
2025-05-29 20:18:44 +0100

    implement map and mapb user commands

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -12,6 +14,7 @@ var supportedCommands = make(map[string]cliCommand)
 
 func main() {
 	createSupportedCommands()
+	config := config{Next: "https://pokeapi.co/api/v2/location-area/"}
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("PokeDex > ")
@@ -22,7 +25,7 @@ func main() {
 
 		cmd, ok := supportedCommands[userCommand]
 		if ok {
-			cmd.callback()
+			cmd.callback(&config)
 		} else {
 			fmt.Printf("Unknown command: '%s', use 'help' for a list of available commands.\n", userCommand)
 		}
@@ -43,32 +46,56 @@ func cleanInput(text string) []string {
 type cliCommand struct {
 	name		string
 	description	string
-	callback func() error
+	callback func(*config) error
 }
 
 func createSupportedCommands() {
 
 	supportedCommands["exit"] = cliCommand{
 		name: "exit",
-		description: "Exits the PokeDex",
+		description: "Exit the PokeDex.",
 		callback: commandExit,
 		}
 
 	supportedCommands["help"] = cliCommand{
 		name: "help",
-		description: "Display this help message",
+		description: "Display this help message.",
 		callback: commandHelp,
 		}
 
+	supportedCommands["map"] = cliCommand{
+		name: "map",
+		description: "List the NEXT 20 locations in the Pokemon world.",
+		callback: commandMap,
+	}
+
+	supportedCommands["mapb"] = cliCommand{
+		name: "mapb",
+		description: "List the PREVIOUS 20 locations in the Pokemon world.",
+		callback: commandMapb,
+	}
+
 }
 
-func commandExit() error {
+type config struct{
+	Count		int		`json:"count"`
+	Next		string	`json:"next"`
+	Previous	string	`json:"previous"`
+	Results		[]result`json:"results"`
+}
+
+type result struct{
+	Name	string	`json:"name"`
+	Url		string	`json:"url"`
+}
+
+func commandExit(c *config) error {
 	fmt.Println("PokeDex exiting...")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(c *config) error {
 	fmt.Println("\nWelcome to the PokeDex!!")
 	fmt.Print("Available commands are as follows:\n\n")
 	for _, cmd := range supportedCommands{
@@ -80,3 +107,36 @@ func commandHelp() error {
 }
 
 
+
+func commandMap(c *config) error {
+	url := c.Next
+	mapHelper(url, c)
+	return nil
+}
+
+func commandMapb(c *config) error {
+	url := c.Previous
+	mapHelper(url, c)
+	return nil
+}
+
+
+func mapHelper(url string, c *config) error {
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	decoder := json.NewDecoder(res.Body)
+	if err := decoder.Decode(&c); err != nil {
+		return err
+	}
+
+	for _, l := range c.Results {
+		fmt.Println(l.Name)
+	}
+
+	return nil
+	
+}
